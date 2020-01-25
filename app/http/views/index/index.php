61da304acd742c15edf0e9d083cd2c5ec7709072
714671404 <714671404@qq.com>
2020-01-25 23:47:24 +0800

    Homepage design

--- app/http/views/index/index.php
+++ app/http/views/index/index.php
@@ -11,10 +11,14 @@
     <div id="app">
         <?php include __DIR__ . '/../layouts/header.php' ?>
 
-        <div class="left">https://juejin.im/post/599970f4518825243a78b9d5</div>
-        <div class="right">123123</div>
-
-        <?php // include __DIR__ . '/../layouts/footer.php'?>
+        <div class="left">
+			https://www.cnblogs.com/kinmos/p/6945885.html <br>
+			https://www.lagou.com/lgeduarticle/81020.html <br>
+			https://www.awaimai.com/128.html <br>
+			https://copyfuture.com/blogs-details/20191213200034043y0xehzw1ykxkpjx <br>
+			https://blog.csdn.net/Zhihua_W/article/details/53167562 <br>
+		</div>
+        <?php include __DIR__ . '/../layouts/footer.php'?>
     </div>
     <script src="/js/main.js" type="text/javascript"></script>
 </body>
