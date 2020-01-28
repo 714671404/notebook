1f262df8c238eab0773e62830cf24a1f6971aa88
714671404 <714671404@qq.com>
2020-01-28 22:02:44 +0800

    '1231'

--- app/http/views/index/index.php
+++ app/http/views/index/index.php
@@ -11,13 +11,46 @@
     <div id="app">
         <?php include __DIR__ . '/../layouts/header.php' ?>
 
-        <div class="left">
-			https://www.cnblogs.com/kinmos/p/6945885.html <br>
-			https://www.lagou.com/lgeduarticle/81020.html <br>
-			https://www.awaimai.com/128.html <br>
-			https://copyfuture.com/blogs-details/20191213200034043y0xehzw1ykxkpjx <br>
-			https://blog.csdn.net/Zhihua_W/article/details/53167562 <br>
-		</div>
+        <section class="main">
+            <div class="content layout">
+                <div class="list-main">
+                    <div class="content-box layout">
+                        <h1 class="content-item-title">
+                            <a href="">30分钟学会 Flex 布局</a>
+                        </h1>
+                        <div class="content-item-rich">
+                            <div class="content-item-other">
+                                <a href="">2020-1-26 15:17:57</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="sib-main">
+                    1111
+                </div>
+
+
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <div style="">
+                    https://www.cnblogs.com/kinmos/p/6945885.html <br>
+                    https://www.lagou.com/lgeduarticle/81020.html <br>
+                    https://www.awaimai.com/128.html <br>
+                    https://copyfuture.com/blogs-details/20191213200034043y0xehzw1ykxkpjx <br>
+                    https://blog.csdn.net/Zhihua_W/article/details/53167562 <br>
+                    https://segmentfault.com/ <br>
+                </div>
+            </div>
+        </section>
+
         <?php include __DIR__ . '/../layouts/footer.php'?>
     </div>
     <script src="/js/main.js" type="text/javascript"></script>
